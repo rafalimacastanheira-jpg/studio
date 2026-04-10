76b8cbbb3b78fc279e130b522cb6314c6f1ee472
Rafael Lima <Rafalimacastanheira@gmail.com>
2026-04-10 18:09:00 +0000

    Integrar Firebase Auth e Firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhMnnFkAg99pwDTIttSaJ1l6uVTzjeiog",
-  authDomain: "studio-7434163111-7759.firebaseapp.com",
-  projectId: "studio-7434163111-7759",
-  storageBucket: "studio-7434163111-7759.firebasestorage.app",
+  apiKey: "AIzaSyDhMnNFkAg99pwDTlttSaJ1l6uVTzje1og",
+  authDomain: "studio-7434163111-77f59.firebaseapp.com",
+  projectId: "studio-7434163111-77f59",
+  storageBucket: "studio-7434163111-77f59.firebasestorage.app",
   messagingSenderId: "358988718023",
-  appId: "1:358988718023:web:6629caa9cb136bcf8c83e2",
+  appId: "1:358988718023:web:5d60879a8d0c806e8c83e2",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
