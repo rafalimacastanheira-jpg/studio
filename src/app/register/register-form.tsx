d58d1c4e1943bdf3f1f77534eede51ca1e69243b
Rafael Lima <Rafalimacastanheira@gmail.com>
2026-04-13 13:36:35 +0000

    integrar firebase

--- src/app/register/register-form.tsx
+++ src/app/register/register-form.tsx
@@ -106,7 +106,7 @@ export function RegisterForm() {
         />
 
         <Button type="submit" className="w-full font-bold">
-        RAFAEL
+        Criar Conta
         </Button>
 
         <p className="text-center text-sm text-muted-foreground">
